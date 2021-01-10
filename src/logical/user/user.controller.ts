55d3fa43c26abed2b9c7d21281a21ece0c537428
fuzzy <17826806525@163.com>
2021-01-10 22:06:52 +0800

    添加了api文档，缺少对传入类型的定义

--- src/logical/user/user.controller.ts
+++ src/logical/user/user.controller.ts
@@ -1,17 +1,23 @@
 import { Body, Controller, Post } from '@nestjs/common';
+import { ApiOperation, ApiPropertyOptional, ApiTags } from '@nestjs/swagger';
 import { UserService } from './user.service'
 
+
+
+@ApiTags('User模块')
 @Controller('user')
 export class UserController {
     // 类定义会创建两个东西：类的实例类型和一个构造函数。 因为类可以创建出类型，所以可以将类作为接口类型
     constructor(private readonly userService: UserService) { }
 
     @Post('find-one')
+    @ApiOperation({ summary: '查询用户是否存在' })
     findOne(@Body() body: any) {
         return this.userService.findOne(body.useraccount)
     }
 
     @Post('register')
+    @ApiOperation({ summary: '注册用户' })
     register(@Body() body: any) {
         return this.userService.register(body)
     }
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   //  Nest 的工厂函数创建了 AppModule
   const app = await NestFactory.create(AppModule);
+
+  const options = new DocumentBuilder()
+    .setTitle("NestJs")
+    .setDescription("nestjs 学习项目")
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api-docs', app, document);
 
   // 全局路由前缀
   // app.setGlobalPrefix('nest-prefix')
